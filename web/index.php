8374a3e55d28a96b835be903a02855d79db18b66
marshguhli <95534364+marshguhli@users.noreply.github.com>
2022-11-26 13:49:36 +0100

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,29 @@
+<?php
+require "../web/controller/LoaderController.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../view/style.css">
+   
+    <title>DATABASE</title>
+    <base href="../web/" >
+</head>
+<body>
+    
+    <div>
+        <h1>~DATABASE~</h1>
+        <a href="../view/add.php" >ADD</a>
+        <a href="../view/select.php" >SHOW</a>
+        <a href="../view/update.php" >UPDATE</a>
+        <a href="../view/delete.php" >DELETE</a>
+    </div> 
+    <div>
+        ~text~
+    </div>
+</body>
+</html>
